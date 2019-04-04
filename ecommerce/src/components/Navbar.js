fe3992216a12f67b282b97b71fb5646c3ba64dd8
uimarshall <uimarshall@gmail.com>
2019-04-05 00:34:46 +0100

    add styles to the Navbar component

--- ecommerce/src/components/Navbar.js
+++ ecommerce/src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export class Navbar extends Component {
 	render() {
 		return (
-			<nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+			<NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
 				{/* https://www.iconfinder.com/icons/1243689/call_phone_icon
     Creative Commons (Attribution 3.0 Unported);
     https://www.iconfinder.com/Makoto_msk */}
@@ -20,13 +22,25 @@ export class Navbar extends Component {
 					</li>
 				</ul>
 				<Link to="/cart" className="ml-auto">
-					<button>
-						<i className="fas fa-cart-plus" /> my cart
-					</button>
+					<ButtonContainer>
+						<span className="mr-2">
+							<i className="fas fa-cart-plus" />
+						</span>
+						my cart
+					</ButtonContainer>
 				</Link>
-			</nav>
+			</NavWrapper>
 		);
 	}
 }
+
+const NavWrapper = styled.nav`
+	background: var(--mainBlue)!important;
+	.nav-link {
+		color: var(--mainWhite) !important;
+		font-size: 1.3rem;
+		text-transform: capitalize;NavWrapper
+	}
+`;
 
 export default Navbar;
